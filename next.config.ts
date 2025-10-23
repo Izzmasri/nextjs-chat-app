8eba2526949f37d3d1bee6d0e3b83f46c4a93691
Izzmasri <izz.masri16@gmail.com>
2025-10-23 18:30:49 +0300

    some fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverExternalPackages: ["ably"],
-    transpilePackages: ["ably"],
-  },
+  serverExternalPackages: ["ably"],
 };
 
 export default nextConfig;
